9c8e759d04ced58d1ea9812cfd7769b5c0b49b00
FireACNC <98172796+FireACNC@users.noreply.github.com>
2024-04-03 17:34:09 -0400

    Add debug statement

--- src/translate/index.js
+++ src/translate/index.js
@@ -6,6 +6,8 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
+    console.log(`${process.env.TRANSLATOR_API}/?content=${postData.content}`);
+    console.log(`${process.env.TRANSLATOR_API}`)
     const response = await fetch(`${process.env.TRANSLATOR_API}/?content=${postData.content}`);
     const data = await response.json();
     return [data.is_english, data.translated_content];
