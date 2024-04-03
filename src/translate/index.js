242d5ae3c2d9f071c1c85d6574597d3bdd34265d
FireACNC <98172796+FireACNC@users.noreply.github.com>
2024-04-03 17:49:51 -0400

    Hard coded url with disabled eslint

--- src/translate/index.js
+++ src/translate/index.js
@@ -6,7 +6,6 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    console.log($os.getenv("TRANSLATOR_API"))
     const response = await fetch(`https://wildcatstranslator-bdjk25ls4q-uc.a.run.app/?content=${postData.content}`);
     const data = await response.json();
     return [data.is_english, data.translated_content];
